9fe49fee53a6c196b2e50964d599f4cfe5177135
Fridolin Kringelbach <fridolinkringelbach@gmail.com>
2024-02-05 19:53:31 +0100

    remove unused functions

--- include/buffer.hpp
+++ include/buffer.hpp
@@ -35,10 +35,6 @@ struct Buffer {
     template<typename T> void fill(size_t size, T* data);
     template<typename T> void fill(std::vector<T> buf);
     template<typename T> void fill(T val);
-    template<typename T> void retrieve(size_t size, T* data);
-    template<typename T> void retrieve(std::vector<T>& buf);
-    template<typename T> void retrieve(T& val);
-
 };
 
 template<typename T>
@@ -57,21 +53,4 @@ void Buffer::fill(std::vector<T> buf) {
 template<typename T>
 void Buffer::fill(T val) {
     fill(1, &val);
-}
-
-template<typename T>
-void Buffer::retrieve(size_t size, T* data) {
-    glBindBuffer(GL_ARRAY_BUFFER, index);
-    glGetBufferSubData(GL_ARRAY_BUFFER, 0, size*sizeof(T), data);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-}
-
-template<typename T>
-void Buffer::retrieve(std::vector<T>& buf) {
-    retrieve(buf.size(), &buf[0]);
-}
-
-template<typename T>
-void Buffer::retrieve(T& val) {
-    retrieve(1, &val);
 }
